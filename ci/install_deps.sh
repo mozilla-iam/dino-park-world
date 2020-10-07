635f11ab7cefdc0db983e8a674d722fae5920254
Florian Merz <me@fiji-flo.de>
2020-10-07 15:50:41 +0200

    move to do.sh

--- ci/install_deps.sh
+++ ci/install_deps.sh
@@ -1,5 +1,2 @@
 HELM_INSTALL_DIR=/bin
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get | bash
-curl -L -o myke https://github.com/fiji-flo/myke/releases/download/0.9.11/myke-0.9.11-x86_64-unknown-linux-musl
-chmod +x ./myke
-mv ./myke /bin/myke
